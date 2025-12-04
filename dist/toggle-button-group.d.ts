10353797bac7e5049a846df9c2a80ff6debe82b0
mwszekely <19625841+mwszekely@users.noreply.github.com>
2025-12-04 13:36:22 -0500

    Fix button group style

--- dist/toggle-button-group.d.ts
+++ dist/toggle-button-group.d.ts
@@ -2,10 +2,9 @@ import { PropsWithChildren } from "react";
 import { Key } from "react-stately";
 import { ButtonStructureProps } from "./util/button-structure";
 export type ToggleButtonGroupProps = ToggleButtonGroupSingleProps | ToggleButtonGroupMultiProps;
-interface ToggleButtonGroupSharedProps {
+interface ToggleButtonGroupSharedProps extends Pick<ButtonStructureProps, 'sizeVariant'> {
     disallowEmptySelection?: boolean | null | undefined;
     orientation?: "vertical" | "horizontal";
-    variantSize?: "sm" | "md" | "lg";
     className?: string;
     disabled?: boolean;
 }
@@ -19,10 +18,10 @@ interface ToggleButtonGroupMultiProps extends ToggleButtonGroupSharedProps {
     selected: Set<Key> | null;
     selectionMode: "multiple";
 }
-export interface ToggleButtonGroupItemProps extends Pick<ButtonStructureProps, "themeVariant" | "themeSpinnerVariant" | "fillVariant"> {
+export interface ToggleButtonGroupItemProps extends Pick<ButtonStructureProps, "themeVariant" | "themeSpinnerVariant" | "fillVariant" | "outsetVariant"> {
     id: Key;
     disabled?: boolean | undefined | null;
 }
 export declare function ToggleButtonGroup(props: PropsWithChildren<ToggleButtonGroupProps>): import("react").JSX.Element | undefined;
-export declare function ToggleButtonGroupItem({ children, id, disabled: disabledU, fillVariant, themeVariant, themeSpinnerVariant, ...props }: PropsWithChildren<ToggleButtonGroupItemProps>): import("react").JSX.Element;
+export declare function ToggleButtonGroupItem({ children, id, disabled: disabledU, fillVariant, themeVariant, outsetVariant, themeSpinnerVariant, ...props }: PropsWithChildren<ToggleButtonGroupItemProps>): import("react").JSX.Element;
 export {};
